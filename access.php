2f472f531875b9810729f321533a832d284c86f1
Sullivan Ford <sullyjhf@hotmail.co.uk>
2014-12-25 00:49:13 +0000

    added google analytics

--- access.php
+++ access.php
@@ -29,14 +29,16 @@
 	//print_r($_SESSION);
 ?>
 	
-	<html><head><title>Login</title>
-	<link href="http://sullivanford.co.uk/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-	<link href="http://sullivanford.co.uk/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
-	<link href="css/reset.css" rel="stylesheet" type="text/css" />
-	<link href="style.css" rel="stylesheet" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
-	<script src="css/jquery-1.11.1.min.js"></script>
-</head>
+	<html>
+	<head>
+		<title>Login</title>
+		<link href="http://sullivanford.co.uk/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+		<link href="http://sullivanford.co.uk/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
+		<link href="css/reset.css" rel="stylesheet" type="text/css" />
+		<link href="style.css" rel="stylesheet" type="text/css" />
+		<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+		<script src="css/jquery-1.11.1.min.js"></script>
+	</head>
 	  <body style="height:100%;">
 	  	<div style="position:fixed; width:174px; height:90px; top:50%; left:50%; margin-left:-87px; margin-top:-45px;">
 			<form method="post">
@@ -53,6 +55,15 @@
 			</form>
 		</div>
 	  </body>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-51676819-2', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</html>
 	
 <?php
